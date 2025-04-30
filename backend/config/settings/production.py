8582fec0d82a1a4461b768fa69ac73d485ab1cc6
Yoiner Duran Rios <yoiner3216898182@gmail.com>
2025-04-30 00:29:38 -0500

    configurando despliegue 7

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -19,7 +19,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["ecommerce-sp.yoyodr.dev"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["ecommerce-sp-api.yoyodr.dev"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
