9c7b4a5849f41736259225638076cab4e3233193
Yoiner Duran Rios <yoiner3216898182@gmail.com>
2025-05-05 00:09:35 -0500

    Siguiendo con el video - 22:20

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import axios from 'axios'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Home from "./containers/Home";
+import Error404 from "./containers/errors/Error404";
 
 function App() {
-  const [error, setError] = useState(null)
-
-  const handleHttpRequest = async () => {
-    try {
-      const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth-token/`, {
-        username: 'test',
-        password: 'test',
-      });
-      console.log(response);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      alert('Failed to fetch data from the server.');
-      setError('Failed to fetch data from the server.');
-    }
-  }
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={handleHttpRequest}>
-          count is 1
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        {/* Error Display */}
+        <Route path="*" element={<Error404 />} />
+
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ frontend/src/hocs/Layout.jsx
@@ -0,0 +1,20 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { Navbar } from '../components/navigation/Navbar';
+import { Footer } from '../components/navigation/Footer';
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Navbar />
+      <ToastContainer
+        autoClose={5000}
+      />
+      {children}
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
